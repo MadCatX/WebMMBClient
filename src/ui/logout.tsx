c078b0b323066b65a4d2edf712e40c5338670e8f
Michal Malý <malym@ibt.cas.cz>
2020-11-18 22:03:54 +0100

    Copy session ID to clipboard on click

--- src/ui/logout.tsx
+++ src/ui/logout.tsx
@@ -10,11 +10,30 @@ import * as React from 'react';
 import { PushButton } from './common/push-button';
 import { AuthQuery } from '../mmb/auth-query';
 
-export class Logout extends React.Component<Logout.Props> {
+interface State {
+    copiedNote: boolean;
+}
+
+export class Logout extends React.Component<Logout.Props, State> {
     constructor(props: Logout.Props) {
         super(props);
 
+        this.state = { copiedNote: false };
+
         this.logOut = this.logOut.bind(this);
+        this.copyIdToClipboard = this.copyIdToClipboard.bind(this);
+    }
+
+    private copyIdToClipboard() {
+        if (!navigator.clipboard) {
+            console.warn('Clipboard API is not supported');
+            return;
+        }
+
+        navigator.clipboard.writeText(this.props.username ?? '');
+
+        this.setState({...this.state, copiedNote: true});
+        setTimeout(() => this.setState({...this.state, copiedNote: false}), 1000);
     }
 
     private logOut() {
@@ -27,9 +46,11 @@ export class Logout extends React.Component<Logout.Props> {
     }
 
     render() {
+        const cpyCls = `green inlined ${this.state.copiedNote ? 'fading-text-visible' : 'fading-text-invisible'}`;
         return (
             <div className='logout-container'>
-                <div className='inlined'>ID: {this.props.username ?? ''}</div>
+                <div className={cpyCls}>Copied!&nbsp;</div>
+                <div className='inlined' onClick={this.copyIdToClipboard}>ID: {this.props.username ?? ''}</div>
                 <PushButton
                     className='pushbutton-chained pushbutton-hc-red'
                     value='Log out'
